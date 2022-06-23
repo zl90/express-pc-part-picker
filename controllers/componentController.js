61c96d80fdb900fc1ff3fb873ddc12cb83a5c51e
zl90 <101400977+zl90@users.noreply.github.com>
2022-06-23 20:11:44 +1000

    Add Delete Component controller and corresponding view

--- controllers/componentController.js
+++ controllers/componentController.js
@@ -291,15 +291,58 @@ exports.component_update_get = function (req, res, next) {
 
 // Handle the updating of a Component on POST
 exports.component_update_post = function (req, res, next) {
-  res.send("Not implemented: component Create POST");
+  res.send("Not implemented: component Update POST");
 };
 
 // Display Delete Component form on GET
 exports.component_delete_get = function (req, res, next) {
-  res.send("Not implemented: component Delete GET");
+  /**
+   * This promise queries the db for the PC Component corresponding to the componentid in the URL
+   */
+  const findComponentById = new Promise((resolve, reject) => {
+    Component.findById(req.params.componentid)
+      .populate("manufacturer")
+      .populate("category")
+      .exec((err, results) => {
+        // Check for db/query errors
+        if (err) {
+          reject(err);
+        }
+
+        // Check for empty results set
+        if (results === null) {
+          // Results are empty, show error
+          const err = new Error("Component not found");
+          err.status = 404;
+          reject(err);
+        }
+
+        // Success, return the PC Component info
+        resolve(results);
+      });
+  });
+
+  findComponentById
+    .then((results) => {
+      // Success, render the PC Component info
+      res.render("component_delete", {
+        title: "Delete Component: \n" + results.name,
+        component: results,
+      });
+    })
+    .catch((err) => {
+      // Catch any erros and pass them along to the error handling middleware
+      return next(err);
+    });
 };
 
 // Handle the deleting of a Component on POST
 exports.component_delete_post = function (req, res, next) {
-  res.send("Not implemented: component Delete POST");
+  Component.findByIdAndRemove(req.params.componentid).exec((err) => {
+    if (err) {
+      return next(err);
+    }
+    // Success, PC component has been deleted. Redirect to the list of PC components:
+    res.redirect("/components");
+  });
 };
