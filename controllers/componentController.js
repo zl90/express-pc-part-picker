6a0954ff4e4235eef09304ece6c04a9f1ea120e0
zl90 <101400977+zl90@users.noreply.github.com>
2022-06-23 21:23:32 +1000

    Add Update Component controller and corresponding view

--- controllers/componentController.js
+++ controllers/componentController.js
@@ -286,13 +286,243 @@ exports.component_create_post = [
 
 // Display Update Component form on GET
 exports.component_update_get = function (req, res, next) {
-  res.send("Not implemented: component Update GET");
+  /**
+   * This promise queries the db for all Categories.
+   * In this case, we'll need Categories to populate our "Select Category" options dropdown.
+   */
+  const getAllCategories = new Promise((resolve, reject) => {
+    Category.find().exec((err, results) => {
+      // check db/query errors
+      if (err) {
+        reject(err);
+      }
+
+      // Success, return category list
+      resolve(results);
+    });
+  });
+
+  /**
+   * This promise queries the db for all Manufacturers.
+   * In this case, we'll need Manufacturers to populate our "Select Manufacturer" options dropdown.
+   */
+  const getAllManufacturers = new Promise((resolve, reject) => {
+    Manufacturer.find().exec((err, results) => {
+      // check db/query errors
+      if (err) {
+        reject(err);
+      }
+
+      // Success, return manufacturer list
+      resolve(results);
+    });
+  });
+
+  /**
+   * This promise queries the db for the PC Component corresponding to the componentid in the URL
+   */
+  const findComponentById = new Promise((resolve, reject) => {
+    Component.findById(req.params.componentid)
+      .populate("manufacturer")
+      .populate("category")
+      .exec((err, results) => {
+        // Check for db/query errors
+        if (err) {
+          reject(err);
+        }
+
+        // Check for empty results set
+        if (results === null) {
+          // Results are empty, show error
+          const err = new Error("Component not found");
+          err.status = 404;
+          reject(err);
+        }
+
+        // Success, return the PC Component info
+        resolve(results);
+      });
+  });
+
+  Promise.all([getAllCategories, getAllManufacturers, findComponentById])
+    .then((results) => {
+      // render the Update Component form.
+      // Let the form know that it is updating an existing PC Component, so display a pre-filled form.
+      res.render("component_form", {
+        title: "Add new PC Part",
+        isUpdating: true,
+        category_list: results[0],
+        manufacturer_list: results[1],
+        component: results[2],
+        component_category: results[2].category,
+        component_manufacturer: results[2].manufacturer,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
 };
 
 // Handle the updating of a Component on POST
-exports.component_update_post = function (req, res, next) {
-  res.send("Not implemented: component Update POST");
-};
+exports.component_update_post = [
+  // Validate and sanitize the POST request values
+  body("name")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Name must be 1-100 characters long")
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 1, max: 1000 })
+    .withMessage("Features must be 1-1000 characters long")
+    .escape(),
+  body("price")
+    .trim()
+    .isFloat({ min: 0, max: 999999 })
+    .withMessage("Price must be numeric value between 0 and 999999")
+    .escape(),
+  body("quantity")
+    .trim()
+    .isInt({ min: 0, max: 9999 })
+    .withMessage("Stock must be numeric value between 0 and 9999")
+    .escape(),
+  body("categoryid", "A Category must be selected").trim().escape(),
+  body("manufacturerid", "A Manufacturer must be selected").trim().escape(),
+
+  function (req, res, next) {
+    // From the POST request, we receive a category ID. Query the db to get the category instance:
+    const findCategoryById = new Promise((resolve, reject) => {
+      Category.findById(req.body.categoryid).exec((err, results) => {
+        // Check for query/db errors
+        if (err) {
+          reject(err);
+        }
+
+        if (results === null) {
+          // No results returned from query, show error
+          const err = new Error("Category not found");
+          err.status = 404;
+          reject(err);
+        }
+
+        // Success, return Category info
+        resolve(results);
+      });
+    });
+
+    // From the POST request, we receive a manufacturer ID. Query the db to get the manufacturer instance:
+    const findManufacturerById = new Promise((resolve, reject) => {
+      Manufacturer.findById(req.body.manufacturerid).exec((err, results) => {
+        // check for db/query errors
+        if (err) {
+          reject(err);
+        }
+
+        if (results === null) {
+          // No results returned from query, show error
+          const err = new Error("Manufacturer not found");
+          err.status = 404;
+          reject(err);
+        }
+
+        // Success, return category info
+        resolve(results);
+      });
+    });
+
+    /**
+     * This promise queries the db for all Categories.
+     * In this case, we'll need Categories to populate our "Select Category" options dropdown.
+     */
+    const getAllCategories = new Promise((resolve, reject) => {
+      Category.find().exec((err, results) => {
+        // check db/query errors
+        if (err) {
+          reject(err);
+        }
+
+        // Success, return category list
+        resolve(results);
+      });
+    });
+
+    /**
+     * This promise queries the db for all Manufacturers.
+     * In this case, we'll need Manufacturers to populate our "Select Manufacturer" options dropdown.
+     */
+    const getAllManufacturers = new Promise((resolve, reject) => {
+      Manufacturer.find().exec((err, results) => {
+        // check db/query errors
+        if (err) {
+          reject(err);
+        }
+
+        // Success, return manufacturer list
+        resolve(results);
+      });
+    });
+
+    Promise.all([
+      findCategoryById,
+      findManufacturerById,
+      getAllCategories,
+      getAllManufacturers,
+    ])
+      .then((results) => {
+        // Manufacturer and Component data successfully queried. Proceed to update the db with new Component values:
+
+        // Extract the validation errors from the POST request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+          // There are errors. Render the Update Component form again with sanitized values/error messages.
+          console.log("errors present!");
+          res.render("component_form", {
+            title: "Add new PC Part",
+            component_category: results[0],
+            component_manufacturer: results[1],
+            component: req.body,
+            isUpdating: true,
+            errors: errors.array(),
+            category_list: results[2],
+            manufacturer_list: results[3],
+          });
+          return;
+        } else {
+          // No validation errors, save the updated Component to the db
+
+          // Create a temporary Component instance with escaped and trimmed data:
+          const newComponent = new Component({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            category: results[0],
+            manufacturer: results[1],
+            _id: req.params.componentid, //This is required, or a new ID will be assigned!
+          });
+
+          // Update the db with new Component instance
+          Component.findByIdAndUpdate(
+            req.params.componentid,
+            newComponent,
+            {},
+            (update_errors, updated_book) => {
+              if (update_errors) {
+                return next(update_errors);
+              }
+              // Component successfully updated. Redirect to it's Detail page
+              console.log(updated_book.url);
+              res.redirect(updated_book.url);
+            }
+          );
+        }
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  },
+];
 
 // Display Delete Component form on GET
 exports.component_delete_get = function (req, res, next) {
