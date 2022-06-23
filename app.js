dd90f94b945e3e0cad609c6dd48d76bbe879db3d
zl90 <101400977+zl90@users.noreply.github.com>
2022-06-23 15:43:20 +1000

    Add Manufacturer Create controller and corresponding view

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const dotenv = require("dotenv/config");
+const bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -26,6 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Manufacturer = require("../models/manufacturer");
 const Component = require("../models/component");
+const { body, validationResult } = require("express-validator");
 
 // Display a list of all manufacturers
 exports.manufacturer_list = function (req, res, next) {
@@ -78,13 +79,78 @@ exports.manufacturer_detail = function (req, res, next) {
 
 // Display the Create Manufacturer form on GET
 exports.manufacturer_create_get = function (req, res, next) {
-  res.send("Not implemented yet: manufacturer create GET");
+  // Just render the Create Manufacturer form.
+  // Let the form know that it's not updating an existing Manufacturer, so display an empty form.
+  res.render("manufacturer_form", {
+    title: "Add new Manufacturer",
+    isUpdating: false,
+  });
 };
 
 // Handle the creation of a Manufacturer on POST
-exports.manufacturer_create_post = function (req, res, next) {
-  res.send("Not implemented yet: manufacturer create POST");
-};
+exports.manufacturer_create_post = [
+  // Validate and sanitize the POST request values (The user inputs 'name' and 'description' values)
+  body("name")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Name must be 1-100 characters long")
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 1, max: 1000 })
+    .withMessage("Description must be 1-1000 characters long")
+    .escape(),
+
+  function (req, res, next) {
+    // Extract the validation errors from the POST request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the Create Manufacturer form again with sanitized values/error messages.
+      res.render("manufacturer_form", {
+        title: "Add new Manufacturer",
+        manufacturer: req.body,
+        isUpdating: false,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // No validation errors, save the new Manufacturer to the db
+
+      // First, check if the new manufacturer already exists in the db:
+      Manufacturer.findOne({ name: req.body.name }).exec((err, results) => {
+        // check for db/query errors
+        if (err) {
+          return next(err);
+        }
+
+        if (results !== null) {
+          // Manufacturer already exists in the db, redirect to it's Detail page
+          res.redirect(results.url);
+        } else {
+          // Manufacturer doesn't exist already, proceed to save it to the db
+
+          // Create the new Manufacturer instance with escaped and trimmed data:
+          const newManufacturer = new Manufacturer({
+            name: req.body.name,
+            description: req.body.description,
+          });
+
+          // Save new manufacturer to the db using Mongoose:
+          newManufacturer.save(function (err) {
+            // check for db/query errors
+            if (err) {
+              return next(err);
+            }
+
+            // Manufacturer successfully saved. Redirect to it's Detail page
+            res.redirect(newManufacturer.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display the Update Manufacturer form on GET
 exports.manufacturer_update_get = function (req, res, next) {
